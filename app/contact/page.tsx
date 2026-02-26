461c3286658429c34421126cab23a28f4e621b68
Fiona Y. <fyeung21@gmail.com>
2026-02-25 16:30:28 -0800

    feat: style contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,15 @@
 export default function Contact() {
   return (
-    <section className="m-4">
-      <h2>contact me</h2>
+    <section className="m-4 my-10">
+      <h2 className="text-2xl my-4">contact me</h2>
       <p>
         Filling this form will send an email to me. You may also reach me through{" "}
         <a href="https://ca.linkedin.com/">linkedin</a> or{" "}
         <a href="https://github.com/fyeung21">github</a>!
       </p>
-      <p>contact form here</p>
+      <article className="my-4">
+        <p>contact form here</p>
+      </article>
     </section>
   );
 }
