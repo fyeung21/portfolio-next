12abd6c4654332734af89c1418baa01cf85b01aa
Fiona Y. <fyeung21@gmail.com>
2026-02-26 02:50:37 -0800

    feat: add conditional for checking skillsList

--- app/components/skillsContainer.tsx
+++ app/components/skillsContainer.tsx
@@ -14,20 +14,22 @@ export default function SkillsContainer({ skillsList }: SkillsContainerProps) {
       <article className="border-b-2 border-gold-300 shadow-underline">
         <h2 className="text-3xl mb-2">skills</h2>
       </article>
-      <ul>
-        {skillsList.map((skillCategory, id) => (
-          <li key={id} className="my-6 mb-8">
-            <h3 className="text-xl">{skillCategory.title}</h3>
-            <ul className="list-inside list-none marker:content-['✦']">
-              {skillCategory.skills.map((skill, id) => (
-                <li key={id}>
-                  <span className="mx-2 leading-8">{skill}</span>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      {skillsList.length !== 0 ? (
+        <ul>
+          {skillsList.map((skillCategory, id) => (
+            <li key={id} className="my-6 mb-8">
+              <h3 className="text-xl">{skillCategory.title}</h3>
+              <ul className="list-inside list-none marker:content-['✦']">
+                {skillCategory.skills.map((skill, id) => (
+                  <li key={id}>
+                    <span className="mx-2 leading-8">{skill}</span>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : null}
     </section>
   );
 }
