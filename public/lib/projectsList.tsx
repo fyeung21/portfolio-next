c1264e98ca04ee4b1d4c4b51c384576a363a0735
Fiona Y. <fyeung21@gmail.com>
2026-02-25 16:57:08 -0800

    feat: add list of projects

---
+++ public/lib/projectsList.tsx
@@ -0,0 +1,50 @@
+export const projectsList = [
+  {
+    id: 0,
+    title: "Catch Oscar",
+    brief: "",
+    img: "",
+    url: "https://fiona-yeung.com",
+    github: "https://github.com/fyeung21/catch-oscar",
+    platform: "Web",
+    tags: ["HTML", "CSS", "JS"],
+    desc: [
+      {
+        id: 0,
+        title: "Game Details & Features",
+        content: [
+          {
+            id: 0,
+            title: "Game Details",
+            content:
+              "‘Catch Oscar’ is a game that has 9 holes where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Eternal Aesthetic",
+    brief: "",
+    img: "",
+    url: "https://fiona-yeung.com",
+    github: "https://github.com/fyeung21/",
+    platform: "Web",
+    tags: ["NextJS", "Tailwind CSS"],
+    desc: [
+      {
+        id: 0,
+        title: "key features",
+        content: [
+          {
+            id: 0,
+            title: "key features",
+            content:
+              "Aesthetician website something something where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
+          },
+        ],
+      },
+    ],
+  },
+];
