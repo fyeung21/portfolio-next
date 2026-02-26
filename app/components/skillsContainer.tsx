92082ed0a98f436a076c9fad60778630f45eedd7
Fiona Y. <fyeung21@gmail.com>
2026-02-26 02:41:24 -0800

    feat: style skills component

--- app/components/skillsContainer.tsx
+++ app/components/skillsContainer.tsx
@@ -10,22 +10,24 @@ export type SkillsContainerProps = {
 
 export default function SkillsContainer({ skillsList }: SkillsContainerProps) {
   return (
-    <section>
-      <h2>skills</h2>
-      <article>
-        <ul>
-          {skillsList.map((skillCategory, id) => (
-            <li key={id}>
-              <h3>{skillCategory.title}</h3>
-              <ul>
-                {skillCategory.skills.map((skill, id) => (
-                  <li key={id}>{skill}</li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
+    <section className="tracking-wide">
+      <article className="border-b-2 border-gold-300 shadow-underline">
+        <h2 className="text-3xl mb-2">skills</h2>
       </article>
+      <ul>
+        {skillsList.map((skillCategory, id) => (
+          <li key={id} className="my-6 mb-8">
+            <h3 className="text-xl">{skillCategory.title}</h3>
+            <ul className="list-inside list-none marker:content-['✦']">
+              {skillCategory.skills.map((skill, id) => (
+                <li key={id}>
+                  <span className="mx-2 leading-8">{skill}</span>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
