e40118fe00f743e8845c2eef595a7b84b7be469a
Fiona Y. <fyeung21@gmail.com>
2026-02-26 15:08:17 -0800

    feat: update project list

--- public/lib/projectsList.tsx
+++ public/lib/projectsList.tsx
@@ -2,49 +2,88 @@ export const projectsList = [
   {
     id: 0,
     title: "Catch Oscar",
-    brief: "",
-    img: "",
+    imgSrc: "/dose-juice.jpg",
+    imgAltDesc: "thumbnail image of catch oscar project",
+    cardBrief: "game that has 9 holes where different neighbourhood cats could appear out of.",
+    cardTech: ["JavaScript", "jQuery"],
+    slug: "catch-oscar",
+    description:
+      "‘Catch Oscar’ is a game that has 9 holes where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
     url: "https://fiona-yeung.com",
     github: "https://github.com/fyeung21/catch-oscar",
     platform: "Web",
-    tags: ["HTML", "CSS", "JS"],
-    desc: [
+    techonologies: ["HTML", "CSS", "JavaScript", "jQuery"],
+    keyFeatures: [
       {
         id: 0,
-        title: "Game Details & Features",
-        content: [
-          {
-            id: 0,
-            title: "Game Details",
-            content:
-              "‘Catch Oscar’ is a game that has 9 holes where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
-          },
-        ],
+        imgSrc: "/ph-shop.jpg",
+        imgAltDesc: "",
+        title: "view movie details",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
+      },
+      {
+        id: 1,
+        imgSrc: "/ph-shop.jpg",
+        imgAltDesc: "",
+        title: "favourite movies",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
+      },
+      {
+        id: 2,
+        imgSrc: "/ph-shop.jpg",
+        imgAltDesc: "",
+        title: "something else",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
       },
     ],
+    screenshots: [
+      { id: 0, imgSrc: "/ph-shop.jpg", imgAltDesc: "", caption: "start page" },
+      { id: 1, imgSrc: "/ph-shop.jpg", imgAltDesc: "", caption: "game board" },
+    ],
+    reflection:
+      "From this project I learned a lot about DOM manipulation, array manipulation, and string interpolation. There were also logic questions I did not know I had to clarify like what happens when randomizing produces the same result as the last result. Not only did I learn how to develop this kind of reaction game, I also learned a little about how designing a game works and how to properly communicate it.",
   },
   {
     id: 1,
     title: "Eternal Aesthetic",
-    brief: "",
-    img: "",
+    imgSrc: "/ph-shop.jpg",
+    imgAltDesc: "thumbnail image of eternal aesthetic project",
+    cardBrief:
+      "An aesthetician clinic site something something different neighbourhood cats could appear out of.",
+    cardTech: ["NextJS", "WordPress"],
+    slug: "eternal-aesthetic",
+    description:
+      "An aesthetician clinic site something something different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
     url: "https://fiona-yeung.com",
-    github: "https://github.com/fyeung21/",
+    github: "https://github.com/fyeung21/catch-oscar",
     platform: "Web",
-    tags: ["NextJS", "Tailwind CSS"],
-    desc: [
+    techonologies: ["NextJS", "TailwindCSS", "WordPress"],
+    keyFeatures: [
       {
         id: 0,
-        title: "key features",
-        content: [
-          {
-            id: 0,
-            title: "key features",
-            content:
-              "Aesthetician website something something where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
-          },
-        ],
+        imgSrc: "/dose-juice.jpg",
+        imgAltDesc: "",
+        title: "view movie details",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
+      },
+      {
+        id: 1,
+        imgSrc: "/dose-juice.jpg",
+        imgAltDesc: "",
+        title: "favourite movies",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
       },
     ],
+    screenshots: [
+      { id: 0, imgSrc: "/dose-juice.jpg", imgAltDesc: "", caption: "home page" },
+      { id: 1, imgSrc: "/dose-juice.jpg", imgAltDesc: "", caption: "about page" },
+    ],
+    reflection:
+      "From this project I learned a lot about DOM manipulation, array manipulation, and string interpolation. There were also logic questions I did not know I had to clarify like what happens when randomizing produces the same result as the last result. Not only did I learn how to develop this kind of reaction game, I also learned a little about how designing a game works and how to properly communicate it.",
   },
 ];
