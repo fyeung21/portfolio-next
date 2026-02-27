5b9282809c91a30996d4e2aa0997e1dbbec57821
Fiona Y. <fyeung21@gmail.com>
2026-02-26 17:44:27 -0800

    refactor: move project single into component

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -1,68 +1,9 @@
-import Image from "next/image";
+import ProjectSingleDetails from "@/app/components/projectSingleDetails";
 
 export default function ProjectSingle() {
   return (
     <section>
-      <article className="relative w-auto h-50 overflow-hidden">
-        <Image
-          className="object-cover"
-          src={"/ph-shop.jpg"}
-          alt={"imgAltDesc"}
-          fill={true}
-          priority
-        />
-      </article>
-      <h2>{"project title"}</h2>
-      <p>{"description"}</p>
-      <h3>technologies</h3>
-      <ul>
-        <li>{"text"}</li>
-        <li>{"text"}</li>
-      </ul>
-      <article>
-        <a href="">live site</a>
-        <a href="">github</a>
-      </article>
-      <article>
-        <h2>key features</h2>
-        <article className="relative w-auto h-50 overflow-hidden">
-          <Image
-            className="object-cover"
-            src={"/ph-shop.jpg"}
-            alt={"imgAltDesc"}
-            fill={true}
-            priority
-          />
-        </article>
-        <p>textskajnflsaf</p>
-        <article className="relative w-auto h-50 overflow-hidden">
-          <Image
-            className="object-cover"
-            src={"/ph-shop.jpg"}
-            alt={"imgAltDesc"}
-            fill={true}
-            priority
-          />
-        </article>
-        <p>textskajnflsaf</p>
-      </article>
-      <article>
-        <h2>screenshots</h2>
-        <article className="relative w-auto h-50 overflow-hidden">
-          <Image
-            className="object-cover"
-            src={"/ph-shop.jpg"}
-            alt={"imgAltDesc"}
-            fill={true}
-            priority
-          />
-        </article>
-        <p>{"caption here"}</p>
-      </article>
-      <article>
-        <h2>reflection</h2>
-        <p>textskajnflsaf</p>
-      </article>
+      <ProjectSingleDetails />
     </section>
   );
 }
