4f441bf1e1d974fe423bdeaf6b17fe9bd7fa833e
Fiona Y. <fyeung21@gmail.com>
2026-02-27 23:46:07 -0800

    feat: add map functions to show lists

--- app/components/projectSingleDetails.tsx
+++ app/components/projectSingleDetails.tsx
@@ -47,62 +47,63 @@ export default function ProjectSingleDetails({
 }: ProjectSingleProps) {
   return (
     <>
-      <article className="relative w-auto h-50 overflow-hidden">
-        <Image className="object-cover" src={imgSrc} alt={imgAltDesc} fill={true} priority />
-      </article>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <h3>platforms</h3>
-      <p>{platform}</p>
-      <h3>technologies</h3>
-      <ul>
-        <li>{technologies[0]}</li>
-        <li>{"text"}</li>
-      </ul>
-      <article>
-        <a href={url}>live site</a>
-        <a href={github}>github</a>
-      </article>
-      <article>
+      <section>
+        <article className="relative w-auto h-50 overflow-hidden">
+          <Image className="object-cover" src={imgSrc} alt={imgAltDesc} fill={true} priority />
+        </article>
+      </section>
+      <section>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <h3>platforms</h3>
+        <p>{platform}</p>
+        <h3>technologies</h3>
+        <ul>
+          {technologies.map((technology, id) => (
+            <li key={id}>{technology}</li>
+          ))}
+        </ul>
+        <article>
+          <a href={url}>live site</a>
+          <a href={github}>github</a>
+        </article>
+      </section>
+      <section>
         <h2>key features</h2>
-        <article className="relative w-auto h-50 overflow-hidden">
-          <Image
-            className="object-cover"
-            src={keyFeatures[0].imgSrc}
-            alt={keyFeatures[0].imgAltDesc}
-            fill={true}
-            priority
-          />
-        </article>
-        <p>{keyFeatures[0].description}</p>
-        <article className="relative w-auto h-50 overflow-hidden">
-          <Image
-            className="object-cover"
-            src={"/ph-shop.jpg"}
-            alt={"imgAltDesc"}
-            fill={true}
-            priority
-          />
-        </article>
-        <p>textskajnflsaf</p>
-      </article>
-      <article>
+        {keyFeatures.map((feature, id) => (
+          <article key={id}>
+            <article className="relative w-auto h-50 overflow-hidden">
+              <Image
+                className="object-cover"
+                src={feature.imgSrc}
+                alt={feature.imgAltDesc}
+                fill={true}
+                priority
+              />
+            </article>
+            <p>{feature.description}</p>
+          </article>
+        ))}
+      </section>
+      <section>
         <h2>screenshots</h2>
-        <article className="relative w-auto h-50 overflow-hidden">
-          <Image
-            className="object-cover"
-            src={screenshots[0].imgSrc}
-            alt={screenshots[0].imgAltDesc}
-            fill={true}
-            priority
-          />
-        </article>
-        <p>{screenshots[0].caption}</p>
-      </article>
-      <article>
-        <h2>reflection</h2>
-        <p>{reflection}</p>
-      </article>
+        {screenshots.map((screen, id) => (
+          <article key={id}>
+            <article className="relative w-auto h-50 overflow-hidden">
+              <Image
+                className="object-cover"
+                src={screen.imgSrc}
+                alt={screen.imgAltDesc}
+                fill={true}
+                priority
+              />
+            </article>
+            <p>{screen.caption}</p>
+          </article>
+        ))}
+      </section>
+      <h2>reflection</h2>
+      <p>{reflection}</p>
     </>
   );
 }
