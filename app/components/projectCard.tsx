fc2e787949549722044c6dcfdd09f2ccf0f1760b
Fiona Y. <fyeung21@gmail.com>
2026-03-01 21:17:49 -0800

    feat: add hover style to project card

--- app/components/projectCard.tsx
+++ app/components/projectCard.tsx
@@ -30,22 +30,22 @@ export default function ProjectCard({
 }: ProjectCardProps) {
   return (
     <article className="py-2">
-      <Link href={`/work/${slug}`}>
-        <section className="relative w-auto h-50 overflow-hidden rounded-2xl">
-          <Image className="object-cover" src={imgSrc} alt={imgAltDesc} fill={true} priority />
-        </section>
-        <section className="my-4">
-          <h3 className="text-2xl">{title}</h3>
-          <p className=" my-2">{cardBrief}</p>
-          <ul className="flex flex-row justify-end">
-            {cardTech.map((techItem, id) => (
-              <li key={id} className="italic mr-2 last:mr-0">
-                {techItem}
-              </li>
-            ))}
-          </ul>
-        </section>
-      </Link>
+      <section className="relative w-auto h-50 overflow-hidden rounded-2xl">
+        <Image className="object-cover" src={imgSrc} alt={imgAltDesc} fill={true} priority />
+      </section>
+      <section className="my-4">
+        <Link href={`/work/${slug}`}>
+          <h3 className="text-2xl hover:underline">{title}</h3>
+        </Link>
+        <p className=" my-2">{cardBrief}</p>
+        <ul className="flex flex-row justify-end">
+          {cardTech.map((techItem, id) => (
+            <li key={id} className="italic mr-2 last:mr-0">
+              {techItem}
+            </li>
+          ))}
+        </ul>
+      </section>
     </article>
   );
 }
