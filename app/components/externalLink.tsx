da9e002d300ba3e2b9a8689aa337885221beab78
Fiona Y. <fyeung21@gmail.com>
2026-03-04 03:35:49 -0800

    feat: style project single for larger screens

--- app/components/externalLink.tsx
+++ app/components/externalLink.tsx
@@ -5,7 +5,7 @@ export type externalLinkProps = {
 
 export default function ExternalLink({ url, label }: externalLinkProps) {
   return (
-    <article className="w-40 m-auto my-8">
+    <article className="w-40 m-auto my-8 md:my-0 md:mb-8">
       <a href={url} className="w-40 m-auto">
         <p className="ring-2 rounded-2xl p-4 text-lg text-center shadow-inset hover:font-semibold hover:shadow-inset-fill">
           {label}
--- app/components/projectSingleDetails.tsx
+++ app/components/projectSingleDetails.tsx
@@ -46,36 +46,38 @@ export default function ProjectSingleDetails({
 }: ProjectSingleProps) {
   return (
     <>
-      <article className="relative h-60 overflow-hidden sm:h-70 md:h-80">
+      <article className="relative h-60 overflow-hidden sm:h-70 md:h-80 lg:h-100">
         <Image className="object-cover" src={imgSrc} alt={imgAltDesc} fill={true} priority />
       </article>
-      <section className="m-4 mb-10">
-        <section>
-          <h2 className="text-3xl my-4">{title}</h2>
-          <p>{description}</p>
-          <article className="my-8">
-            <h3 className="text-xl capitalize">technologies</h3>
-            <ul className="list-inside list-none marker:content-['✦'] sm:flex sm:flex-row">
-              {technologies.map((technology, id) => (
-                <li key={id}>
-                  <span className="mx-2 leading-8">{technology}</span>
-                </li>
-              ))}
-            </ul>
+      <section className="m-4 mb-10 md:mx-0">
+        <section className="md:flex md:flex-row md:justify-between">
+          <article>
+            <h2 className="text-3xl my-4 lg:text-4xl">{title}</h2>
+            <p>{description}</p>
+            <article className="my-8">
+              <h3 className="text-xl capitalize">technologies</h3>
+              <ul className="list-inside list-none marker:content-['✦'] sm:flex sm:flex-row">
+                {technologies.map((technology, id) => (
+                  <li key={id}>
+                    <span className="mx-2 leading-8">{technology}</span>
+                  </li>
+                ))}
+              </ul>
+            </article>
           </article>
-          <article className="py-2 sm:flex sm:flex-row sm:justify-evenly">
+          <article className="py-2 sm:flex sm:flex-row sm:justify-evenly md:flex-col md:my-4 md:justify-normal md:px-4">
             {url ? <ExternalLink url={url} label={"live site"} /> : null}
             {github ? <ExternalLink url={github} label={"github"} /> : null}
           </article>
         </section>
         <section className="my-12">
           <article className="border-b-2 border-gold-300 shadow-underline my-4">
-            <h2 className="text-2xl my-2">key features</h2>
+            <h2 className="text-2xl my-2 lg:text-3xl">key features</h2>
           </article>
-          <article className="sm:grid sm:grid-cols-2 sm:gap-8">
+          <article className="sm:grid sm:grid-cols-2 sm:gap-8 lg:gap-16">
             {keyFeatures.map((feature, id) => (
               <article key={id} className="py-4">
-                <article className="relative w-auto h-60 sm:h-50 rounded-2xl overflow-hidden">
+                <article className="relative w-auto h-60 sm:h-50 lg:h-60 rounded-2xl overflow-hidden">
                   <Image
                     className="object-cover"
                     src={feature.imgSrc}
@@ -84,19 +86,21 @@ export default function ProjectSingleDetails({
                     priority
                   />
                 </article>
-                <h3 className="font-bold capitalize my-4">{feature.title}</h3>
-                <p>{feature.description}</p>
+                <article className="md:px-2">
+                  <h3 className="font-bold capitalize my-4">{feature.title}</h3>
+                  <p>{feature.description}</p>
+                </article>
               </article>
             ))}
           </article>
         </section>
         <section>
           <article className="border-b-2 border-gold-300 shadow-underline my-4">
-            <h2 className="text-2xl my-2">screenshots</h2>
+            <h2 className="text-2xl my-2 lg:text-3xl">screenshots</h2>
           </article>
           {screenshots.map((screen, id) => (
             <article key={id} className="py-4">
-              <article className="relative w-auto h-60 sm:h-90 rounded-2xl overflow-hidden">
+              <article className="relative w-auto h-60 sm:h-90 lg:h-120 rounded-2xl overflow-hidden">
                 <Image
                   className="object-cover"
                   src={screen.imgSrc}
@@ -110,7 +114,7 @@ export default function ProjectSingleDetails({
           ))}
         </section>
         <article className="border-b-2 border-gold-300 shadow-underline my-4">
-          <h2 className="text-2xl my-2">reflection</h2>
+          <h2 className="text-2xl my-2 lg:text-3xl">reflection</h2>
         </article>
         <p>{reflection}</p>
       </section>
