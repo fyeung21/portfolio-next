1595ed1edbdb265e039e4301585fe52c8afe653d
Fiona Y. <fyeung21@gmail.com>
2026-03-04 04:01:39 -0800

    feat: add styling for larger screens to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,15 @@
 export default function Contact() {
   return (
-    <section className="mx-4 my-10 px-4">
-      <h2 className="text-2xl my-4">contact me</h2>
+    <section className="mx-4 my-20 px-4 sm:w-120 sm:mx-auto md:w-180 lg:w-220">
+      <h2 className="text-2xl my-4 lg:text-3xl">contact me</h2>
       <p>
         Filling this form will send an email to me. You may also reach me through{" "}
         <a href="https://ca.linkedin.com/" className="font-bold hover:underline">
-          linkedin
+          <span>linkedin</span>
         </a>{" "}
         or{" "}
         <a href="https://github.com/fyeung21" className="font-bold hover:underline">
-          github
+          <span>github</span>
         </a>
         !
       </p>
