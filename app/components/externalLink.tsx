92ac197bca8623d1494f8049a10dea761ff208dd
Fiona Y. <fyeung21@gmail.com>
2026-03-04 00:31:30 -0800

    feat: update button hover styles

--- app/components/externalLink.tsx
+++ app/components/externalLink.tsx
@@ -7,7 +7,7 @@ export default function ExternalLink({ url, label }: externalLinkProps) {
   return (
     <article className="w-40 m-auto my-8">
       <a href={url} className="w-40 m-auto">
-        <p className="ring-2 rounded-2xl p-4 text-lg text-center hover:font-bold hover:shadow-inset">
+        <p className="ring-2 rounded-2xl p-4 text-lg text-center shadow-inset hover:font-semibold hover:shadow-inset-fill">
           {label}
         </p>
       </a>
