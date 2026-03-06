701cfc993998b7c58f103eb0022d4c5a6b0a17ca
Fiona Y. <fyeung21@gmail.com>
2026-03-05 23:45:48 -0800

    fix: add element tags to project list

--- public/lib/projectsList.tsx
+++ public/lib/projectsList.tsx
@@ -43,7 +43,7 @@ export const projectsList = [
       { id: 1, imgSrc: "/ph-shop.jpg", imgAltDesc: "", caption: "game board" },
     ],
     reflection:
-      "From this project I learned a lot about DOM manipulation, array manipulation, and string interpolation. There were also logic questions I did not know I had to clarify like what happens when randomizing produces the same result as the last result. Not only did I learn how to develop this kind of reaction game, I also learned a little about how designing a game works and how to properly communicate it. <h4>Future Considerations</h4> <ul><li>Improve on UI and UX design</li><li>Code refactoring, for example, change some if statements to switch cases.</li><li>Countdown after unpausing</li><li>Add a Leaderboard</li><li>Small bugs to clean up</li></ul>",
+      "<p>From this project I learned a lot about DOM manipulation, array manipulation, and string interpolation. There were also logic questions I did not know I had to clarify like what happens when randomizing produces the same result as the last result. Not only did I learn how to develop this kind of reaction game, I also learned a little about how designing a game works and how to properly communicate it.</p><h4>Future Considerations</h4><ul><li>Improve on UI and UX design</li><li>Code refactoring, for example, change some if statements to switch cases.</li><li>Countdown after unpausing</li><li>Add a Leaderboard</li><li>Small bugs to clean up</li></ul>",
   },
   {
     id: 1,
