24db3cf502747a726cf0f78a72df762fbf88d0c0
Fiona Y. <fyeung21@gmail.com>
2026-03-08 16:15:21 -0700

    feat: style project nav

--- app/components/projectNav.tsx
+++ app/components/projectNav.tsx
@@ -3,7 +3,8 @@ import { projectsList } from "@/public/lib/projectsList";
 
 export default function ProjectNav() {
   return (
-    <section className="max-w-100 m-auto sm:max-w-none md:w-180 lg:w-220">
+    <section className="max-w-100 my-16 m-auto sm:max-w-none md:w-180 lg:w-220">
+      <h2 className="text-2xl my-2 mx-4 font-bold italic sm:mx-0">my other work</h2>
       <Carousel itemList={projectsList} />
     </section>
   );
