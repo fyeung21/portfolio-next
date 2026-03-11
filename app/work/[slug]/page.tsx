dd518b63048683edc407260ae9a760b86195f36e
Fiona Y. <fyeung21@gmail.com>
2026-03-11 01:31:01 -0700

    chore: delete unused code

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -3,24 +3,10 @@
 import ProjectSingleDetails from "@/app/components/projectSingleDetails";
 import { projectsList } from "@/public/lib/projectsList";
 import ProjectNav from "@/app/components/projectNav";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-// import { useEffect } from "react";
 
 export default function ProjectSingle() {
-  // useEffect(() => {
-  //   AOS.init({
-  //     duration: 2000,
-  //   });
-  // }, []);
-
   return (
-    <section
-      className="max-w-100 m-auto sm:max-w-none md:w-180 lg:w-220"
-      // data-aos="fade-up"
-      // data-aos-easing="ease-in"
-      // data-aos-duration="1500"
-    >
+    <section className="max-w-100 m-auto sm:max-w-none md:w-180 lg:w-220">
       <ProjectSingleDetails
         title={projectsList[0].title}
         imgSrc={projectsList[0].imgSrc}
