0b35a17fad51532a2852330f57ac0bcabdfb17ab
Fiona Y. <fyeung21@gmail.com>
2026-03-11 02:54:18 -0700

    feat: update link styles

--- app/components/externalLink.tsx
+++ app/components/externalLink.tsx
@@ -7,7 +7,7 @@ export default function ExternalLink({ url, label }: externalLinkProps) {
   return (
     <article className="w-40 m-auto my-8 sm:my-0 sm:mb-8">
       <a href={url} className="w-40 m-auto">
-        <p className="ring-2 rounded-2xl p-4 text-lg text-center shadow-inset hover:font-semibold hover:shadow-inset-fill">
+        <p className="ring-2 rounded-2xl p-4 font-semibold text-lg text-center shadow-inset hover:ring-3 hover:shadow-inset-fill hover:text-shadow-md hover:text-shadow-indigo-950">
           {label}
         </p>
       </a>
