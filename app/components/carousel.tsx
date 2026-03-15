8a12dfde9306ae1c350de75e505fdabf8caf5a88
Fiona Y. <fyeung21@gmail.com>
2026-03-14 22:40:36 -0700

    feat: add styling when item is snapped in carousel

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { EmblaCarouselType } from "embla-carousel";
+import ClassNames from "embla-carousel-class-names";
 import Image from "next/image";
 import Link from "next/link";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
@@ -20,7 +21,7 @@ export type CarouselProps = {
 };
 
 export default function Carousel({ itemList }: CarouselProps) {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [ClassNames()]);
 
   const goToPrev = () => emblaApi?.scrollPrev();
   const goToNext = () => emblaApi?.scrollNext();
@@ -35,12 +36,12 @@ export default function Carousel({ itemList }: CarouselProps) {
 
   return (
     <section className="embla relative">
-      <article className="embla__viewport overflow-hidden py-4 w-180 m-auto" ref={emblaRef}>
+      <article className="embla__viewport overflow-hidden py-4 " ref={emblaRef}>
         <section className="embla__container flex flex-row touch-pan-y touch-pinch-zoom">
           {itemList.map((item, id) => (
-            <article key={id} className="embla__slide basis-64 grow-0 shrink-0 mx-4">
-              <Link href={item.slug} className="hover:underline">
-                <article className="relative h-40 rounded-2xl overflow-hidden hover:ring-2 hover:border-gold-300 hover:shadow-underline">
+            <article key={id} className="embla__slide basis-64 grow-0 shrink-0 mx-4 opacity-100">
+              <Link href={item.slug}>
+                <article className="relative h-40 rounded-2xl overflow-hidden hover:ring-3 hover:border-amber-200 hover:shadow-card">
                   <Image
                     className="object-cover"
                     src={item.imgSrc}
