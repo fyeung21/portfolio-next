d43125c30b2162755df383872257e816fe3c665d
Fiona Y. <fyeung21@gmail.com>
2026-03-18 23:52:44 -0700

    feat: style contact form

--- app/components/contactForm.tsx
+++ app/components/contactForm.tsx
@@ -6,20 +6,32 @@ const list = [
 
 export default function ContactForm() {
   return (
-    <section>
+    <section className="m-auto rounded-xl px-8 py-4 shadow-inset ring-2">
       <form name="contact" method="POST" data-netlify="true">
         {list.map((item, id) => (
-          <p key={id}>
-            <label>{item.label}</label>
-            <input type={item.type} name={item.name} />
+          <p key={id} className="my-4 flex flex-col">
+            <label className="mb-2 font-semibold">{item.label}</label>
+            <input
+              type={item.type}
+              name={item.name}
+              className="rounded-xl p-2 ring-2 focus:shadow-inset focus-visible:ring-4 focus-visible:ring-amber-200"
+            />
           </p>
         ))}
-        <p>
-          <label>{"message"}</label>
-          <textarea name={"message"}></textarea>
+        <p className="my-2 flex flex-col">
+          <label className="my-2 font-semibold">message</label>
+          <textarea
+            name="message"
+            className="h-40 rounded-xl p-2 ring-2 focus:shadow-inset focus-visible:ring-4 focus-visible:ring-amber-200"
+          ></textarea>
         </p>
-        <p>
-          <button type="submit">{"send"}</button>
+        <p className="flex justify-center">
+          <button
+            className="m-auto mt-10 mb-8 w-40 rounded-xl py-2 text-center text-lg font-semibold shadow-inset ring-2 hover:shadow-inset-fill hover:ring-3 hover:text-shadow-indigo-950 hover:text-shadow-md"
+            type="submit"
+          >
+            send
+          </button>
         </p>
       </form>
     </section>
