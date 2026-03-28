4723fd0d83a76b1a8658a811599c1f26cd46c7ed
Fiona Y. <fyeung21@gmail.com>
2026-03-27 23:52:23 -0700

    fix: make all hover styles apply when hovering over card

--- app/components/projectCard.tsx
+++ app/components/projectCard.tsx
@@ -30,29 +30,36 @@ export default function ProjectCard({
   return (
     <article className="py-2">
       <Link href={`/work/${slug}`}>
-        <section className="relative h-50 w-auto overflow-hidden rounded-2xl duration-400 ease-in hover:border-amber-200 hover:shadow-card hover:ring-3 hover:transition-all hover:duration-400 hover:ease-out lg:h-60">
-          <Image
-            className="object-cover duration-500 ease-in hover:scale-105 hover:transition-all hover:duration-400 hover:ease-out"
-            src={imgSrc}
-            alt={imgAltDesc}
-            fill={true}
-            priority
-          />
+        <section className="group">
+          <section className="relative h-50 w-auto overflow-hidden rounded-2xl duration-400 ease-in group-hover:border-amber-200 group-hover:shadow-card group-hover:ring-3 group-hover:transition-all group-hover:duration-400 group-hover:ease-out lg:h-60">
+            <Image
+              className="object-cover duration-500 ease-in group-hover:scale-105 group-hover:transition-all group-hover:duration-400 group-hover:ease-out"
+              src={imgSrc}
+              alt={imgAltDesc}
+              fill={true}
+              priority
+            />
+          </section>
+
+          <section className="relative h-45 px-2 py-4">
+            <h3 className="text-2xl">
+              <span className="duration-400 ease-in group-hover:border-b-2 group-hover:transition-all group-hover:duration-400 group-hover:ease-out">
+                {title}
+              </span>
+            </h3>
+            <p className="my-2">{cardBrief}</p>
+            <article className="absolute right-2 bottom-0">
+              <ul className="flex flex-row justify-end">
+                {cardTech.map((techItem, id) => (
+                  <li key={id} className="mr-2 italic last:mr-0">
+                    {techItem}
+                  </li>
+                ))}
+              </ul>
+            </article>
+          </section>
         </section>
       </Link>
-      <section className="relative h-45 px-2 py-4">
-        <h3 className="text-2xl">{title}</h3>
-        <p className="my-2">{cardBrief}</p>
-        <article className="absolute right-2 bottom-0">
-          <ul className="flex flex-row justify-end">
-            {cardTech.map((techItem, id) => (
-              <li key={id} className="mr-2 italic last:mr-0">
-                {techItem}
-              </li>
-            ))}
-          </ul>
-        </article>
-      </section>
     </article>
   );
 }
