d0bf6c424d198cfcd6c3e70ed3091ff4cbb4f029
Fiona Y. <fyeung21@gmail.com>
2026-04-19 23:38:56 -0700

    feat: retrieve slug in ProjectNav

--- app/components/projectNav.tsx
+++ app/components/projectNav.tsx
@@ -4,9 +4,15 @@ import Carousel from "@/app/components/carousel";
 import { projectsList } from "@/public/lib/projectsList";
 import { useEffect } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 
-export default function ProjectNav() {
+export default async function ProjectNav({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
   useEffect(() => {
     AOS.init({
       duration: 2000,
@@ -23,6 +29,8 @@ export default function ProjectNav() {
       <h2 className="mx-4 my-2 text-2xl font-bold italic md:mx-0">
         my other work
       </h2>
+
+      {/* remove slug from projectList before passing it to Carousel */}
       <Carousel projectsList={Object.values(projectsList)} />
     </section>
   );
