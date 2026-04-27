8ec70142ea906e24a215cc4d9a8176c7a7f97292
Fiona Y. <fyeung21@gmail.com>
2026-04-26 22:28:03 -0700

    feat: add placeholder project

--- public/lib/projectsList.tsx
+++ public/lib/projectsList.tsx
@@ -113,18 +113,61 @@ export const projectsList = {
     reflection:
       "From this project I learned a lot about DOM manipulation, array manipulation, and string interpolation. There were also logic questions I did not know I had to clarify like what happens when randomizing produces the same result as the last result. Not only did I learn how to develop this kind of reaction game, I also learned a little about how designing a game works and how to properly communicate it.",
   },
-  "test-something": {
-    title: "test something",
-    imgSrc: "/dose-juice.jpg",
-    imgAltDesc: "thumbnail image of catch oscar project",
+  "pebble-counter": {
+    title: "pebble counter",
+    imgSrc: "/ph-shop.jpg",
+    imgAltDesc: "thumbnail image of pebble project",
     cardBrief:
       "game that has 9 holes where different neighbourhood cats could appear out of.",
     cardTech: ["JavaScript", "jQuery"],
-    slug: "test-something",
+    slug: "pebble-counter",
     description:
       "‘Catch Oscar’ is a game that has 9 holes where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
-    url: "https://fiona-yeung.com",
-    github: "https://github.com/fyeung21/catch-oscar",
+    url: "",
+    github: "https://github.com/fyeung21/pebble-counter",
+    technologies: ["HTML", "CSS", "JavaScript", "jQuery"],
+    keyFeatures: [
+      {
+        imgSrc: "/ph-shop.jpg",
+        imgAltDesc: "",
+        title: "view movie details",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
+      },
+      {
+        imgSrc: "/ph-shop.jpg",
+        imgAltDesc: "",
+        title: "favourite movies",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
+      },
+      {
+        imgSrc: "/ph-shop.jpg",
+        imgAltDesc: "",
+        title: "something else",
+        description:
+          "A movie database website with functions for filtering movies, login, and favouriting.",
+      },
+    ],
+    screenshots: [
+      { imgSrc: "/ph-shop.jpg", imgAltDesc: "", caption: "start page" },
+      { imgSrc: "/ph-shop.jpg", imgAltDesc: "", caption: "game board" },
+    ],
+    reflection:
+      "<p>From this project I learned a lot about DOM manipulation, array manipulation, and string interpolation. There were also logic questions I did not know I had to clarify like what happens when randomizing produces the same result as the last result. Not only did I learn how to develop this kind of reaction game, I also learned a little about how designing a game works and how to properly communicate it.</p><h4>Future Considerations</h4><ul><li>Improve on UI and UX design</li><li>Code refactoring, for example, change some if statements to switch cases.</li><li>Countdown after unpausing</li><li>Add a Leaderboard</li><li>Small bugs to clean up</li></ul>",
+  },
+  minty: {
+    title: "minty",
+    imgSrc: "/dose-juice.jpg",
+    imgAltDesc: "thumbnail image of minty project",
+    cardBrief:
+      "game that has 9 holes where different neighbourhood cats could appear out of.",
+    cardTech: ["JavaScript", "jQuery"],
+    slug: "minty",
+    description:
+      "‘Catch Oscar’ is a game that has 9 holes where different neighbourhood cats could appear out of. Only one cat will show at one time and will disappear after a couple seconds.",
+    url: "",
+    github: "https://github.com/fyeung21/minty",
     technologies: ["HTML", "CSS", "JavaScript", "jQuery"],
     keyFeatures: [
       {
